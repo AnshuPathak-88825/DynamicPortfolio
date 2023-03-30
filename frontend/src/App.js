f978257b17f354cd678be324bfe435b501ed4d09
ankit-11525 <ankitpathak11525@gmail.com>
2023-03-30 16:26:43 +0530

    Projects part done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from "./Components/Home/Home.jsx";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import About from "./Components/About/About";
+import Projects from "./Components/Projects/Projects";
 function App() {
   return (
     <Router>
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer/>
     </Router>
---
+++ frontend/src/Components/Projects/Projects.jsx
@@ -0,0 +1,70 @@
+import { Button, Typography } from "@mui/material";
+import React from "react";
+import "./Projects.css";
+import { AiOutlineProject } from "react-icons/ai";
+import { Delete } from "@mui/icons-material";
+import { FaRegSmileWink } from "react-icons/fa";
+
+const ProjectCard = ({
+  url,
+  projectImage,
+  projectTitle,
+  description,
+  technologies,
+  isAdmin = false,
+}) => {
+
+  return (
+    <>
+      <a href={url} className="projectCard" target="blank">
+        <div>
+          <img src={projectImage} alt="Project" />
+          <Typography variant="h5">{projectTitle}</Typography>
+        </div>
+        <div>
+          <Typography variant="h4"> About Project</Typography>
+          <Typography>{description}</Typography>
+          <Typography variant="h6">Tech Stack: {technologies}</Typography>
+        </div>
+      </a>
+
+      {isAdmin && (
+        <Button
+          style={{ color: "rgba(40,40,40,0.7)" }}
+        //   onClick={() => deleteHandler(id)}
+        >
+          <Delete />
+        </Button>
+      )}
+    </>
+  );
+};
+
+const Projects = () => {
+    const projects=[1,2,3];
+  return (
+    <div className="projects">
+      <Typography variant="h3">
+        Projects <AiOutlineProject />
+      </Typography>
+
+      <div className="projectsWrapper">
+        {projects.map((item) => (
+          <ProjectCard
+            url="https://ankit-11525.github.io/e-commerce_redstore.github.io/"
+            projectImage="https://user-images.githubusercontent.com/75976169/193442392-76cea148-631a-4d45-9cf3-2db27c32c1b0.png"
+            projectTitle="RedStore - An eCommerce Website"
+            description="RedStore is a fully responsive ecommerce website, built using HTML, CSS, and JavaScript."
+            technologies="html css js react"
+          />
+        ))}
+      </div>
+
+      <Typography variant="h3" style={{ font: "100 1.2rem 'Ubuntu Mono'" }}>
+        All The Projects Shown Above  <FaRegSmileWink />
+      </Typography>
+    </div>
+  );
+};
+
+export default Projects;
