8d358d3f8a7d8b46bdb9699c6f6a11df7c4f4f71
Ankit-11525 <ankitpathak11525@gmail.com>
2023-06-20 15:59:13 +0530

    cloudinary connected done

--- backend/controller/User.js
+++ backend/controller/User.js
@@ -1,137 +1,199 @@
-import {User} from "../model/User.js";
-import jwt from 'jsonwebtoken'
+import { User } from "../model/User.js";
+import jwt from "jsonwebtoken";
+import cloudinary from "cloudinary";
 import { sendMail } from "../midllewares/sendMail.js";
-export const login= async (req,res)=>
-{
-    try{
-        const {email,password}=req.body;
-        const user=await User.findOne({email,password});
-        // if user is invalid 
-        if(!user)
-        {
-            return res.status(400).json({
-                success:false,
-                message:"Invalid email or password",
-            }) 
-        }
-        // if user correct then go for generatetoken
-
-        const token=jwt.sign({_id:user._id },process.env.JWT_SECRET);
-        res.status(200).cookie("token",token,{
-            expire:new Date(Date.now()+600000),
-            httpOnly:true,
-        }).json({
-            success:true,
-            message:"Logged In Successfully"
-        })
-    }catch(error)
-    {
-        return res.status(400).json({
-            success:false,
-            message:error.message,
-        })
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email, password });
+    // if user is invalid
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
     }
+    // if user correct then go for generatetoken
 
-}
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    res
+      .status(200)
+      .cookie("token", token, {
+        expire: new Date(Date.now() + 600000),
+        httpOnly: true,
+      })
+      .json({
+        success: true,
+        message: "Logged In Successfully",
+      });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
+export const logout = async (req, res) => {
+  // only we send response
+  try {
+    res
+      .status(200)
+      .cookie("token", null, {
+        expire: new Date(Date.now()),
+        httpOnly: true,
+      })
+      .json({
+        success: true,
+        message: "Logged Out Successfully",
+      });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
+// i am not creating register as i don't want to call api for multiple , we just create manually user in mongodb
 
-export const logout= async (req,res)=>
-{
-    // only we send response
-    try{
-        res.status(200).cookie("token",null,{
-            expire:new Date(Date.now()),
-            httpOnly:true,
-        }).json({
-            success:true,
-            message:"Logged Out Successfully"
-        })
-    }catch(error)
-    {
-        return res.status(400).json({
-            success:false,
-            message:error.message,
-        })
+export const getUser = async (req, res) => {
+  try {
+    // ek user dhudhte hai
+    // get functionn automatically call ho jayega so we dont know username and password use SELECT method to avoid
+    const user = await User.findOne().select("-password -email");
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const myProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const contact = async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+    const userMessage = `Hello , I am ${name}. My email is ${email}. My message is ${message}. `;
+    await sendMail(userMessage);
+    return res.status(200).json({
+      success: true,
+      message: "Message Sent Successfully",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    const { name, email, skills, projects } = req.body;
+    if (name) {
+      user.name = name;
     }
-
-}
-
-// i am not creating register as i don't want to call api for multiple , we just create manually user in mongodb 
-
-
-export const getUser= async(req,res)=>
-{
-    try{
-        // ek user dhudhte hai
-        // get functionn automatically call ho jayega so we dont know username and password use SELECT method to avoid
-        const user = await User.findOne().select("-password -email");
-
-        res.status(200).json({
-          success: true,
-          user,
+    if (email) {
+      user.email = email;
+    }
+    if (skills) {
+      if (skills.image1) {
+        await cloudinary.v2.uploader.destroy(user.skills.image1.public_id);
+        const myCloud = await cloudinary.v2.uploader.upload(skills.image1, {
+          folder: "portfolio",
         });
-    }
-    catch(error){
-        return res.status(400).json({
-            success:false,
-            message:error.message,
-        })
-    }
-}
 
+        user.skills.image1 = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
 
-
-export const myProfile= async(req,res)=>
-{
-    try{
-        const user = await User.findById(req.user._id);
-        res.status(200).json({
-          success: true,
-          user,
+      if (skills.image2) {
+        await cloudinary.v2.uploader.destroy(user.skills.image2.public_id);
+        const myCloud = await cloudinary.v2.uploader.upload(skills.image2, {
+          folder: "portfolio",
         });
-    }
-    catch(error){
-        return res.status(400).json({
-            success:false,
-            message:error.message,
-        })
-    }
-}
 
+        user.skills.image2 = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
 
-export const contact= async(req,res)=>
-{
-    try{
-        const {name,email,message}=req.body;
-        const userMessage=`Hello , I am ${name}. My email is ${email}. My message is ${message}. `;
-        await sendMail(userMessage);
-        return res.status(200).json({
-            success: true,
-            message: "Message Sent Successfully",
-          });
-   
-    }
-    catch(error){
-        return res.status(400).json({
-            success:false,
-            message:error.message,
-        })
-    }
-}
+      if (skills.image3) {
+        await cloudinary.v2.uploader.destroy(user.skills.image3.public_id);
+        const myCloud = await cloudinary.v2.uploader.upload(skills.image3, {
+          folder: "portfolio",
+        });
 
+        user.skills.image3 = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
 
+      if (skills.image4) {
+        await cloudinary.v2.uploader.destroy(user.skills.image4.public_id);
+        const myCloud = await cloudinary.v2.uploader.upload(skills.image4, {
+          folder: "portfolio",
+        });
 
-export const updateUser= async(req,res)=>
-{
-    try{
-        const user = await User.findById(req.user._id);
-        
+        user.skills.image4 = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+
+      if (skills.image5) {
+        await cloudinary.v2.uploader.destroy(user.skills.image5.public_id);
+        const myCloud = await cloudinary.v2.uploader.upload(skills.image5, {
+          folder: "portfolio",
+        });
+
+        user.skills.image5 = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+
+      if (skills.image6) {
+        await cloudinary.v2.uploader.destroy(user.skills.image6.public_id);
+        const myCloud = await cloudinary.v2.uploader.upload(skills.image6, {
+          folder: "portfolio",
+        });
+
+        user.skills.image6 = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
     }
-    catch(error){
-        return res.status(400).json({
-            success:false,
-            message:error.message,
-        })
-    }
-}
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import { app } from "./app.js";
 import dotenv from "dotenv";
-import {connectDatabase} from './config/database.js';
+import cloudinary from "cloudinary";
+import { connectDatabase } from "./config/database.js";
 dotenv.config({ path: "./backend/config/config.env" });
 connectDatabase();
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 const port = process.env.PORT;
 // console.log(port);
 app.listen(4000, () => {
