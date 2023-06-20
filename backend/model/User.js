f7d1cbf6b237af2307bfa02003ae11eed97f8c40
Ankit-11525 <ankitpathak11525@gmail.com>
2023-06-20 11:33:16 +0530

    usermodel done

---
+++ backend/model/User.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+import { emitWarning } from "process";
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please Enter Email"],
+    unique: true,
+  },
+  password: String,
+  timline: [{ title: String, description: String, date: Date }],
+  skills: {
+    image1: {
+      public_id: String,
+      url: String,
+    },
+    image2: {
+      public_id: String,
+      url: String,
+    },
+    image3: {
+      public_id: String,
+      url: String,
+    },
+    image4: {
+      public_id: String,
+      url: String,
+    },
+    image5: {
+      public_id: String,
+      url: String,
+    },
+    image6: {
+      public_id: String,
+      url: String,
+    },
+    youtube: [
+      {
+        url: String,
+        title: String,
+        image: {
+          public_id: String,
+          url: String,
+        },
+      },
+    ],
+
+    projects: [
+      {
+        url: String,
+        title: String,
+        image: {
+          public_id: String,
+          url: String,
+        },
+        description: String,
+        techStack: String,
+      },
+    ],
+
+    about: {
+      name: String,
+      title: String,
+      subtitle: String,
+      description: String,
+      quote: String,
+      avatar: {
+        public_id: String,
+        url: String,
+      },
+    },
+  },
+});
+export const user = mongoose.model("user", userSchema);
